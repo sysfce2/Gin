b6b028981456a4a63b7bcd30001260f0ba3d9afa
Roland Rabien <figbug@gmail.com>
2022-11-06 13:31:28 -0800

    Webp encoding

--- modules/gin_graphics/formats/gin_webpimageformat.cpp
+++ modules/gin_graphics/formats/gin_webpimageformat.cpp
@@ -10,6 +10,7 @@ JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wzero-as-null-pointer-constant")
 extern "C"
 {
 #include "../3rdparty/webp/webp/decode.h"
+#include "../3rdparty/webp/webp/encode.h"
 }
 
 JUCE_END_IGNORE_WARNINGS_GCC_LIKE
@@ -49,17 +50,49 @@ juce::Image WEBPImageFormat::decodeImage (juce::InputStream& input)
     if (! WebPGetInfo ((uint8_t*)mb.begin(), mb.getSize(), &w, &h))
         return {};
     
-    juce::Image img (juce::Image::ARGB, w, h, true);
-    
-    juce::Image::BitmapData data (img, juce::Image::BitmapData::readWrite);
-    
-    WebPDecodeBGRAInto ((uint8_t*)mb.begin(), mb.getSize(), data.data, data.size, data.lineStride);
+    WebPBitstreamFeatures features;
+    std::memset (&features, 0, sizeof (features));
+    if (WebPGetFeatures ((uint8_t*)mb.begin(), mb.getSize(), &features) != VP8_STATUS_OK)
+        return {};
 
+    juce::Image img (features.has_alpha ? juce::Image::ARGB : juce::Image::RGB, w, h, true);
+
+    juce::Image::BitmapData data (img, juce::Image::BitmapData::readWrite);
+
+    if (data.pixelFormat == juce::Image::ARGB)
+        WebPDecodeBGRAInto ((uint8_t*)mb.begin(), mb.getSize(), data.data, data.size, data.lineStride);
+    else
+        WebPDecodeBGRInto ((uint8_t*)mb.begin(), mb.getSize(), data.data, data.size, data.lineStride);
+    
     return img;
 }
 
-bool WEBPImageFormat::writeImageToStream (const juce::Image& sourceImage, juce::OutputStream& dst)
+bool WEBPImageFormat::writeImageToStream (const juce::Image& img, juce::OutputStream& dst)
 {
-    juce::ignoreUnused (sourceImage, dst);
+    juce::Image::BitmapData data (img, juce::Image::BitmapData::readOnly);
+    
+    if (loseless)
+    {
+        uint8_t* output = nullptr;
+        auto size = WebPEncodeLosslessBGRA (data.data, img.getWidth(), img.getHeight(), data.lineStride, &output);
+        if (size > 0 && output != nullptr)
+        {
+            dst.write (output, size);
+            WebPFree (output);
+            return true;
+        }
+    }
+    else
+    {
+        uint8_t* output = nullptr;
+        auto size = WebPEncodeBGRA (data.data, img.getWidth(), img.getHeight(), data.lineStride, quality, &output);
+        if (size > 0 && output != nullptr)
+        {
+            dst.write (output, size);
+            WebPFree (output);
+            return true;
+        }
+    }
+
     return false;
 }
